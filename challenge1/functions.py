851a359e3fd778a7199cdb6910313b5c9e465241
FelipeSPB <felipebiancalana@outlook.com>
2019-06-14 22:22:05 -0300

    'ex'

---
+++ challenge1/functions.py
@@ -0,0 +1,30 @@
+def count_dictionary(string):
+    string = ' '.join(string)
+    string_split = string.split()
+    string_sort = sorted(string_split)
+    output_dictionary = {}
+    for i in range(0,len(string_sort)):
+        output_dictionary[string_sort[i]] = string_sort.count(string_sort[i])
+        
+    return output_dictionary
+
+
+def count_sum(string):
+    output=""
+    verifier = ""
+    until = len(string)
+
+    for index in range(0,until):
+        if index==0:
+            verifier += string[index]
+            continue 
+        elif string[index] != string[index - 1]:
+            output += str(verifier.count(string[index -1 ]))
+            output += string[index -1]
+            verifier = ""
+        verifier += string[index]
+    output += str(verifier.count(string[index]))
+    output += string[index]
+    return str(output)
+
+
